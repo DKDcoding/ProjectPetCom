05ce3efa0ba930db6f8118f11cf939d8f5348d37
Divyajot Kaur <77391270+DKDcoding@users.noreply.github.com>
2023-12-05 18:50:33 -0800

    Add files via upload
    Task 3 xml and java files

---
+++ pettrainers.java
@@ -0,0 +1,14 @@
+package com.example.projectpetcom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pettrainers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pettrainers);
+    }
+}
